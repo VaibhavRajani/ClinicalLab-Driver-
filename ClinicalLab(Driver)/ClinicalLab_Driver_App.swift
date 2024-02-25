84e24fd2ba49063393de98980697358d90dbaf70
Vaibhav Rajani <101408581+VaibhavRajani@users.noreply.github.com>
2024-02-25 17:53:01 -0500

    Initial Commit

---
+++ ClinicalLab(Driver)/ClinicalLab_Driver_App.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicalLab_Driver_App.swift
+//  ClinicalLab(Driver)
+//
+//  Created by Vaibhav Rajani on 2/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ClinicalLab_Driver_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
